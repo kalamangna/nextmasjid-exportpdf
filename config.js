6e36a335d22a5ff230f166a24a0c5576a73ac6d5
Abd Dzuljalali Wal Ikram <dzuljalali.walikram@provisms.com>
2021-07-10 23:18:38 +0800

    Update pdf template

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // baseUrl: `https://nextmasjid-exportpdf.herokuapp.com`,
-  baseUrl: `http://localhost:3000`,
+  baseUrl: `https://nextmasjid-exportpdf.herokuapp.com`,
+  // baseUrl: `http://localhost:3000`,
   port: 3000,
   GMAP_API_KEY: "AIzaSyC2Kcxwvpo5WJ2pM9Dbr2dKCFjS2wZwFoM",
 };
